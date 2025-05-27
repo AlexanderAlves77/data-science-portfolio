f8cf2edbb98fbb219489626d8f8c7803c4b6cfcc
Alexander Alves <vendas77.rj@gmail.com>
2025-05-26 21:18:22 -0300

    Adicionando Algoritimos Inteligente de Buscas

---
+++ algoritmos_inteligentes_busca_python/busca_sem_informacao/Largura.py
@@ -0,0 +1,33 @@
+from Fila import Fila
+
+class Largura:
+    def __init__(self, inicio, objetivo):
+        self.inicio = inicio
+        self.inicio.visitado = True
+        self.objetivo = objetivo
+        self.fronteira = Fila(20)
+        self.fronteira.enfileirar(inicio)
+        self.achou = False
+
+    def buscar(self):
+        primeiro = self.fronteira.getPrimeiro()
+        print(f'Primeiro: {primeiro.nome}')
+
+        if primeiro == self.objetivo:
+            self.achou = True
+        else:
+            temp = self.fronteira.desenfileirar()
+            print(f'Desenfileirou: {temp.nome}')
+            for a in primeiro.adjacentes:
+                print(f'Verificando se já visitado: {a.cidade.nome}')
+                if a.cidade.visitado == False:
+                    a.cidade.visitado = True
+                    self.fronteira.enfileirar(a.cidade)
+            if self.fronteira.numeroElementos > 0:
+                Largura.buscar(self)
+
+
+from Mapa import Mapa
+mapa = Mapa()
+largura = Largura(mapa.portoUniao, mapa.curitiba)
+largura.buscar()
